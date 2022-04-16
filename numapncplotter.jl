81075212586c3aa4f776be563560524cf40f7a92
SIMONE MARRAS <simone@SIMONEs-MacBook-Air.local>
2022-04-16 11:53:24 -0400

    push

--- numapncplotter.jl
+++ numapncplotter.jl
@@ -1,8 +1,14 @@
-using ClimateBase
 using NetCDF
-using Plots
 
-filepath = "/Users/simone/Work/numa-git/GitLab_NPS/IMEXbranch/numa3d/myruns/AMR/case13/WORKING-AMR-TC-35th-conference/"
+import PyPlot as plt
+using NumPyArrays
+using PyCall
+
+tri = pyimport("matplotlib.tri")
+np  = pyimport("numpy")
+
+filepath = "/Users/simone/Work/numa-git/GitLab_NPS/IMEXbranch/numa3d/myruns/amr/WORKING-AMR-TC-35th-conference"
+#filepath = "/Users/simone/Work/numa-git/GitLab_NPS/IMEXbranch/numa3d/myruns/AMR/case13/WORKING-AMR-TC-35th-conference/"
 filename = "case13_AMR=3_0.025000_set2nc_cgd_ark2_schur_3d_p4est_0001.nc"
 
 
@@ -17,8 +23,24 @@ ycoords = ncread(fn,"y")
 zcoords = ncread(fn,"z")
 theta  = ncread(fn,"theta")
 
-@info size(theta) size(xcoords)
-contour(x=xcoords, y=ycoords, z=theta)
+@show xmin = minimum(xcoords)
+@show xmax = maximum(xcoords)
+@show ymin = minimum(ycoords)
+@show ymax = maximum(ycoords)
+@show zmin = minimum(zcoords)
+@show zmax = maximum(zcoords)
+
+triang       = tri.Triangulation(xcoords, ycoords)
+interpolator = tri.LinearTriInterpolator(triang, theta)
 
 
+# Create grid values first.
+ngridx = 100
+ngridy = 200
+xi = np.linspace(xmin, xmax, ngridx)
+yi = np.linspace(xmin, ymax, ngridy)
+Xi, Yi = np.meshgrid(xi, yi)
+zi = interpolator(Xi, Yi)
+#
+#@info size(theta) size(xcoords)
 
