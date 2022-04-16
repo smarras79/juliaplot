9a9e0565616e1bd7362ab9c12b10b4d173eca553
Simone <smarras@njit.edu>
2022-04-16 14:52:29 -0400

    WIP: using julia Triangulate.jl to build the interpolation

--- numapncplotter.jl
+++ numapncplotter.jl
@@ -1,37 +1,82 @@
 using NetCDF
 
-import PyPlot as plt
+#import PyPlot as plt
+using PyPlot
 using NumPyArrays
 using PyCall
+using Triangulate
+using Test
 
-tri = pyimport("matplotlib.tri")
-np  = pyimport("numpy")
+function myfindall(condition, x)
+    results = Int[]
 
-filepath = "/Users/simone/Work/numa-git/GitLab_NPS/IMEXbranch/numa3d/myruns/amr/WORKING-AMR-TC-35th-conference"
-#filepath = "/Users/simone/Work/numa-git/GitLab_NPS/IMEXbranch/numa3d/myruns/AMR/case13/WORKING-AMR-TC-35th-conference/"
-filename = "case13_AMR=3_0.025000_set2nc_cgd_ark2_schur_3d_p4est_0001.nc"
+    for i in 1:length(x)
+        if condition(x[i])
+            push!(results, i)
+        end
+    end
+    return results
+end
+
+function example_convex_hull(;Plotter=nothing,n=10,raster=10)
+    triin=Triangulate.TriangulateIO()
+    triin.pointlist=hcat(unique([ Cdouble[rand(1:raster)/raster, rand(1:raster)/raster] for i in 1:n])...)
+    @show size(triin.pointlist) #QUIIII now modify this to use my XSLICE ad YSLICE
+    display(triin)
+    (triout, vorout)=triangulate("Q", triin)
+    display(triout)
+    plot_in_out(Plotter,triin,triout,title="Convex hull")
+    @test numberofpoints(triin)==numberofpoints(triout)
+    @test numberoftriangles(triout)>0
+end
+
+function main()
+    tri = pyimport("matplotlib.tri")
+    np  = pyimport("numpy")
+
+    #filepath = "/Users/simone/Work/numa-git/GitLab_NPS/IMEXbranch/numa3d/myruns/amr/WORKING-AMR-TC-35th-conference"
+    filepath = "/Users/simone/Work/numa-git/GitLab_NPS/IMEXbranch/numa3d/myruns/AMR/case13/WORKING-AMR-TC-35th-conference/"
+    filename = "case13_AMR=3_0.025000_set2nc_cgd_ark2_schur_3d_p4est_0001.nc"
 
 
-fn = joinpath(filepath, filename)
-@info fn
-ncinfo(fn)
+    fn = joinpath(filepath, filename)
+    @info fn
+    ncinfo(fn)
 
 
-nop     = ncread(fn,"nopx")
-xcoords = ncread(fn,"x")
-ycoords = ncread(fn,"y")
-zcoords = ncread(fn,"z")
-theta  = ncread(fn,"theta")
+    nop     = ncread(fn,"nopx")
+    xcoords = ncread(fn,"x")
+    ycoords = ncread(fn,"y")
+    zcoords = ncread(fn,"z")
+    theta   = ncread(fn,"theta")
 
-@show xmin = minimum(xcoords)
-@show xmax = maximum(xcoords)
-@show ymin = minimum(ycoords)
-@show ymax = maximum(ycoords)
-@show zmin = minimum(zcoords)
-@show zmax = maximum(zcoords)
+    @show xmin = minimum(xcoords)
+    @show xmax = maximum(xcoords)
+    @show ymin = minimum(ycoords)
+    @show ymax = maximum(ycoords)
+    @show zmin = minimum(zcoords)
+    @show zmax = maximum(zcoords)
 
-triang       = tri.Triangulation(xcoords, ycoords)
-interpolator = tri.LinearTriInterpolator(triang, theta)
+    #
+    # Extract indexes from zcoords where zcoords = zmin
+    #
+    #zslice_idx = myfindall(z -> z == zmin, zcoords);
+    zslice_idx = findall(z -> z == zmin, zcoords)
+    #
+    # extract x and y subarrays at zslice_idx
+    #
+    xslice = xcoords[zslice_idx]
+    yslice = ycoords[zslice_idx]
+    
+    example_convex_hull(Plotter=PyPlot,n=10,raster=10);
+    
+    #triang = Triangulate(xslice, yslice)
+    #triang       = tri.Triangulation(xslice, yslice)
+    #interpolator = tri.LinearTriInterpolator(triang, theta)
+    
+    #=
+   
+
 
 
 # Create grid values first.
@@ -43,4 +88,10 @@ Xi, Yi = np.meshgrid(xi, yi)
 zi = interpolator(Xi, Yi)
 #
 #@info size(theta) size(xcoords)
+
+=#
+    
+end
+
+main();
 
